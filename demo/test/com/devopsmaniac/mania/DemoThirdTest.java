9e3e5f7c80494c8355489f87d8f910ca78798ddb
Tom O'Neill <Tom.ONeill@servicenow.com>
2018-08-16 16:54:51 -0700

    HP-5: Two new tests, bumped failure rate down

---
+++ demo/test/com/devopsmaniac/mania/DemoThirdTest.java
@@ -0,0 +1,13 @@
+package com.devopsmaniac.mania;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoThirdTest {
+
+	@Test
+	public void somethingRandom() {
+		assertEquals( 1, Demo.somethingRandom() );
+	}
+}
