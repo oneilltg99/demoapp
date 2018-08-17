6b744a7f302ae8ec43aaacc4f817db6254c3d47e
Tom O'Neill <Tom.ONeill@servicenow.com>
2018-08-17 10:14:10 -0700

    HP-5: Added a test with @Ignore

--- demo/test/com/devopsmaniac/mania/DemoSecondTest.java
+++ demo/test/com/devopsmaniac/mania/DemoSecondTest.java
@@ -3,11 +3,18 @@ package com.devopsmaniac.mania;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Ignore;
 
 public class DemoSecondTest {
 
 	@Test
 	public void somethingRandom() {
+		assertEquals( 1, Demo.somethingRandom() );
+	}
+
+	@Test
+	@Ignore
+	public void ignoreMePlease() {
 		assertEquals( 1, Demo.somethingRandom() );
 	}
 }
