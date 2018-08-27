497b90af2fbed3eaf5be4b0a86dde7c8fe89c4c6
Tom O'Neill <Tom.ONeill@servicenow.com>
2018-08-27 15:26:52 -0700

    trying to get screenshot

--- demo/test/com/devopsmaniac/mania/ScreenshotTest.java
+++ demo/test/com/devopsmaniac/mania/ScreenshotTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
@@ -11,14 +12,22 @@ import org.junit.runners.model.Statement;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import org.apache.commons.io.FileUtils;
+
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.OutputType;
+
+// trying to get screenshot, but not working for me (locally or remotely)
+
 public class ScreenshotTest {
 	private static WebDriver driver;
 
@@ -28,10 +37,10 @@ public class ScreenshotTest {
 	@Test
 	public void checkRandomSitePass() throws InterruptedException, MalformedURLException {
 
-		driver = new RemoteWebDriver(
-                new URL("http://selenium.sndevops.xyz:4444/wd/hub"),
-                DesiredCapabilities.firefox());
-		//driver = new FirefoxDriver();
+		// driver = new RemoteWebDriver(
+        //         new URL("http://selenium.sndevops.xyz:4444/wd/hub"),
+        //         DesiredCapabilities.firefox());
+		driver = new FirefoxDriver();
 
         //Launch the Online Store Website
 		driver.get("http://www.google.com");
@@ -83,12 +92,22 @@ class ScreenshotTestRule implements MethodRule {
 
             public void captureScreenshot(String fileName) {
                 try {
-                    new File("target/surefire-reports/").mkdirs(); // Ensure directory is there
-                    FileOutputStream out = new FileOutputStream("target/surefire-reports/screenshot-" + fileName + ".png");
-                    out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
-                    out.close();
+					if (driver != null) {
+	                    new File("target/surefire-reports/").mkdirs(); // Ensure directory is there
+	                    FileOutputStream out = new FileOutputStream("target/surefire-reports/screenshot-" + fileName + ".png");
+	                    out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+	                    out.close();
+					}
+
+
+
                 } catch (Exception e) {
-                    // No need to crash the tests if the screenshot fails
+					StringWriter sw = new StringWriter();
+					PrintWriter pw = new PrintWriter(sw);
+					e.printStackTrace(pw);
+					String sStackTrace = sw.toString(); // stack trace as a string
+					System.out.println(sStackTrace);
+					System.out.println("failed on screenshot: " + e);
                 }
             }
         };
