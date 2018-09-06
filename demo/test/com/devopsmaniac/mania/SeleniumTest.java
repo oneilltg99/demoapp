35c2ca8fd6c04653b755ce3f5c7090197ed28cf8
Nehal Chaudhary <Nehal.chaudhary@servicenow.com>
2018-09-05 17:01:19 -0700

    random combud

--- demo/test/com/devopsmaniac/mania/SeleniumTest.java
+++ demo/test/com/devopsmaniac/mania/SeleniumTest.java
@@ -25,7 +25,7 @@ public class SeleniumTest {
         assertEquals(driver.getTitle(), "Google");
         
 		//Wait for 5 Sec
-		Thread.sleep(5000);
+		Thread.sleep(5000); 
 		
         // Close the driver
         driver.quit();
