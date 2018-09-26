0e055a5650b3fd1e0db1e3c27a84793d9ed73e9a
Nehal Chaudhary <Nehal.chaudhary@servicenow.com>
2018-09-26 15:37:42 -0700

    fail the build

--- demo/test/com/devopsmaniac/mania/DemoFourthTest.java
+++ demo/test/com/devopsmaniac/mania/DemoFourthTest.java
@@ -9,22 +9,22 @@ public class DemoFourthTest {
 
 	@Test 
 	public void betterNam() {
-		assertEquals( 1, 1);
+		assertEquals( 1, Demo.somethingRandom());
 	}
 
 	@Test
 	@Ignore
 	public void skippingMe() {
-		assertEquals( 1, 1);
+		assertEquals( 1, Demo.somethingRandom());
 	}
 
 	@Test
         public void yooooooo() {
-                assertEquals( 1, 1);
+                assertEquals( 1, Demo.somethingRandom());
         }
 
 	@Test
         public void goodBye() {
-                assertEquals( 1, 1 );
+                assertEquals( 1, Demo.somethingRandom() );
         }
 }
