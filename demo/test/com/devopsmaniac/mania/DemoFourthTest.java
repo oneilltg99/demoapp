b58461569784866a6d2d44be9b20ec1c4b4c1fb6
Nehal Chaudhary <Nehal.chaudhary@servicenow.com>
2018-11-06 10:56:01 -0800

    somehting ramdon

--- demo/test/com/devopsmaniac/mania/DemoFourthTest.java
+++ demo/test/com/devopsmaniac/mania/DemoFourthTest.java
@@ -13,22 +13,22 @@ public class DemoFourthTest {
 
 	@Test 
 	public void betterNam() {
-		assertEquals( 1, 1);
+		assertEquals( Demo.somethingRandom(), 1);
 	}
 
 	@Test
 	@Ignore
 	public void skippingMe() {
-		assertEquals( 1, 1);
+		assertEquals( Demo.somethingRandom(), 1);
 	}
 
 	@Test
         public void yooooooo() {
-                assertEquals( 1, 1);
+                assertEquals( Demo.somethingRandom(), 1);
         }
 
 	@Test
         public void goodBye() {
-                assertEquals( 1, 1);
+                assertEquals( Demo.somethingRandom(), 1);
         }
 }
