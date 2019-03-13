5c206518dbfe77a5629cb5fb5104906495a1bd0f
Tom O'Neill <Tom.ONeill@servicenow.com>
2019-03-13 13:45:40 -0700

    abc

--- demo/test/com/devopsmaniac/mania/DemoSecondTest.java
+++ demo/test/com/devopsmaniac/mania/DemoSecondTest.java
@@ -1,11 +1,5 @@
 package com.devopsmaniac.mania;
 
-
-// pushing when the repo was not discovered but the tool was connected worked fine
-// now I've disconnected the tool. repo exists. going to push again
-// that correctly created the payload and marked it as not connected
-// now I'll reconnect and push again
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
