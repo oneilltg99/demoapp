3174802abdc28244724efc4f54f057ec47ab5290
Tom O'Neill <Tom.ONeill@servicenow.com>
2019-03-27 17:36:19 -0700

    hhh

--- demo/src/com/devopsmaniac/mania/Demo.java
+++ demo/src/com/devopsmaniac/mania/Demo.java
@@ -12,7 +12,7 @@ public class Demo {
 		} catch (InterruptedException e) {
 			
 		}
-		if (Math.random() < 0.04)
+		if (Math.random() < 0.01)
 			return 0;
 		return 1;
 	}
--- demo/test/com/devopsmaniac/mania/DemoTest.java
+++ demo/test/com/devopsmaniac/mania/DemoTest.java
@@ -5,7 +5,6 @@ package com.devopsmaniac.mania;
 
 
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
